7c7376df75158e304cbf464b73d1a8403ca69bc4
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-04-14 15:32:19 +0200

    updated setup metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.0.0"
+version = "1.1.0"
 
 setup(
     name="felicity.serial",
@@ -35,23 +35,10 @@ setup(
         "uvicorn==0.16.0",
         "jinja2==3.1.2",
         "ndg-httpsclient",
-        "pyopenssl"
+        "pyopenssl",
         "pyasn1"
     ],   
     python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Implementers',
-        'License :: OSI Approved :: MIT License',  
-        'Operating System :: POSIX :: Linux', 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
